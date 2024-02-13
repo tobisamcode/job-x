477df543263b1b6edfb341882b275b9c92e755b8
tobisamcode <samueloluwatobiloba48@gmail.com>
2024-02-13 13:13:54 +0100

    in the dev mode

--- app/index.js
+++ app/index.js
@@ -1,10 +1,40 @@
-import { Text, View } from "react-native";
+import { Text, View, ScrollView, SafeAreaView } from "react-native";
+import { Stack, useRouter } from "expo-router";
+
+import { COLORS, FONT, icons, images, SIZES } from "../constants";
+import {
+  Nearbyjobs,
+  Popularjobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from "../components";
+import styles from "./index.style";
 
 const Home = () => {
   return (
-    <View>
-      <Text> Home </Text>
-    </View>
+    <SafeAreaView style={styles.safeAreaView}>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" />
+          ),
+          headerTitle: "",
+        }}
+      ></Stack.Screen>
+
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={styles.container}>
+          <Welcome />
+          <Popularjobs />
+          {/* <Nearbyjobs /> */}
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
